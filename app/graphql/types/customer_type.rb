8aa24d036e178b785f39588b3e2f1fc4069b2379
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2022-03-22 18:55:01 +0300

    Add remaining query types
    - Fix the field type uuid to expect ID
    - Make customer type to return country type as opposed to country id
    
    Signed-off-by: Denis Ngugi Gathondu <denis.gathondu@yahoo.com>

--- app/graphql/types/customer_type.rb
+++ app/graphql/types/customer_type.rb
@@ -5,7 +5,7 @@ module Types
     field :id, ID, null: false
     field :first_name, String
     field :last_name, String
-    field :country_id, Types::UuidType, null: false
+    field :country, Types::CountryType, null: false
     field :address, String
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -21,5 +21,47 @@ module Types
     def country(id:)
       Country.find(id)
     end
+
+    # Currencies
+    field :currencies, [Types::CurrencyType], null: false
+    field :currency, Types::CurrencyType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def currencies
+      Currency.all
+    end
+
+    def currency(id:)
+      Currency.find(id)
+    end
+
+    # customers
+    field :customers, [Types::CustomerType], null: false
+    field :customer, Types::CustomerType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def customers
+      Customer.all
+    end
+
+    def customer(id:)
+      Customer.find(id)
+    end
+
+    # transactions
+    field :transactions, [Types::TransactionType], null: false
+    field :transaction, Types::TransactionType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def transactions
+      Transaction.all
+    end
+
+    def transaction(id:)
+      Transaction.find(id)
+    end
   end
 end
