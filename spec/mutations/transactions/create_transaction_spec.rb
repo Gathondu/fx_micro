25e2bbe12243fab3f6decaae1fbb748110407374
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2022-03-24 06:08:48 +0300

    refactor test to - add tests to query for a single transaction after it has been created
    Signed-off-by: Denis Ngugi Gathondu <denis.gathondu@yahoo.com>

--- spec/mutations/transactions/create_transaction_spec.rb
+++ spec/mutations/transactions/create_transaction_spec.rb
@@ -33,6 +33,7 @@ RSpec.describe Mutations::CreateTransaction, type: :graphql do
       }
     GQL
   end
+
   let(:transactions_query) do
     <<~GQL
       query {
@@ -43,12 +44,42 @@ RSpec.describe Mutations::CreateTransaction, type: :graphql do
     GQL
   end
 
+  let(:transaction_query) do
+    <<~GQL
+      query($id: ID!) {
+        transaction(id: $id) {
+          customer {
+            id
+          }
+          inputAmount
+          inputCurrency {
+            id
+          }
+          outputAmount
+          outputCurrency {
+            id
+          }
+        }
+      }
+    GQL
+  end
+
   let(:customer) { create(:customer) }
   let(:input_currency) { create(:input_currency) }
   let(:output_currency) { create(:output_currency) }
 
-  it 'creates a transaction successfully' do
-    expected_outcome = {
+  before do
+    @outcome = execute_graphql(
+      mutation,
+      variables: {
+        customerId: customer.id,
+        inputAmount: '30',
+        inputCurrencyId: input_currency.id,
+        outputCurrencyId: output_currency.id
+      },
+    )
+
+    @expected_outcome = {
       "customer" => {
         "id" => customer.id
       },
@@ -61,18 +92,20 @@ RSpec.describe Mutations::CreateTransaction, type: :graphql do
         "id" => output_currency.id
       },
     }
-    outcome = execute_graphql(
-      mutation,
-      variables: {
-        customerId: customer.id,
-        inputAmount: '30',
-        inputCurrencyId: input_currency.id,
-        outputCurrencyId: output_currency.id
-      },
-    )
-    transactions = execute_graphql(transactions_query)
+  end
 
-    expect(outcome['data']['createTransaction']['transaction']).to eq expected_outcome
+  it 'creates a transaction successfully' do
+    expect(@outcome['data']['createTransaction']['transaction']).to eq @expected_outcome
+  end
+
+  it 'persists the transactions and they can be queried' do
+    transactions = execute_graphql(transactions_query)
+    transaction = execute_graphql(
+      transaction_query,
+      variables: { id:  transactions['data']['transactions'][0]['id']}
+    )
+
     expect(transactions['data']['transactions'].count).to eq 1
+    expect(transaction['data']['transaction']).to eq @expected_outcome
   end
 end
